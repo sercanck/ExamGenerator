ab5b460dafa73d0bbabbd47b72bac3c71bdcdb6f
Sercan Cikintoglu <cikintoglus@itu.edu.tr>
2025-08-14 12:24:38 +0300

    RequiredPackages/Noquestioncandidate

--- exam.py
+++ exam.py
@@ -4,16 +4,19 @@ import re
 import random
 
 
-pn_questions  = "./questions/"
-template_name = "template.tex"
-question_list = []
+pn_questions  = "./questions/"  #Name of the question folder
+template_name = "template.tex"  #Name of the exam's template
+output_name   = "exam.tex"      #Name of the exam
 
+question_list = []  
+packages_list = dict()
+questions_all = ""        
+packages_all  = ""
 
 def read_file(file_path):
     with open(file_path, 'r', encoding='utf-8') as file: #read file
         content = file.read()
     file.close()
-    #print(content)
     return content
     
     
@@ -27,10 +30,30 @@ def extract_field(content, field):
     match = re.search(pattern, content, re.DOTALL)
     return match.group(1).strip() if match else None
 
-# Example usage
-#result = parse_file("your_file.txt")
-#print("Keywords:", result["Keywords"])
-#print("Difficulty:", result["Difficulty"])
+def extract_packages(content,plist):
+    """
+    Extract text between {Requiredpackages: ... END}
+    Parse command and argument part seperately
+    command= usepackages, tikzlibrary... etc.
+    arguments= tikz,... etc.
+    plist =  package list
+    """
+    content_temp=extract_field(content, "Requiredpackages")
+    for line in content_temp.splitlines():
+      command, arguments = line.split(": ")
+
+    #TODO: Requiredpackage yoksa...
+
+
+    if command in list(packages_list.keys()): #If the command exists
+      for package in arguments.split(","):
+        if package not in plist[command]:
+          plist[command] = plist[command]+","+package
+    else:
+      plist[command]=arguments
+
+    return plist
+
 
 with open('exam_structure','r') as file_structure:
     for line in file_structure.readlines(): #Loop over target question
@@ -39,7 +62,7 @@ with open('exam_structure','r') as file_structure:
 
       """Find suitable questions"""
       for qn in sorted(os.listdir(pn_questions)):
-        if qn in question_list:
+        if qn in question_list: #If this question is already selected.
           continue
       
         fn=pn_questions+qn
@@ -47,36 +70,46 @@ with open('exam_structure','r') as file_structure:
         Keywords   = extract_field(content,"Keywords")
         Difficulty = extract_field(content,"Difficulty")
  
-        #Tüm dosyaları okumak zahmetli grep gibi bir sey olsa iyi is gorurdu
+        #TODO:Tüm dosyaları okumak zahmetli grep gibi bir sey olsa iyi is gorurdu
         
         if (target_difficulty==Difficulty) and (target_keyword in Keywords):           
           possible_questions.append(qn)
-          #TODO: target_keyword birden fazla olabilir
+          #TODO: target_keyword birden fazla olabilir           
 
-      #TODO: if possible_questions is empty do something...
-        
-      question_list.append(random.choice(possible_questions))   
-
+      if not possible_questions: 
+        print("Cannot find a suitable question candidate for "+question+"!!!")  #if possible_questions is empty do something...
+      else:  
+        question_list.append(random.choice(possible_questions))      
                   
-  
-questions_all = ""        
-#Loop tex birleştir        
+
+"""Merge questions and packages"""        
 for qn in question_list:
+  
   fn=pn_questions+qn
   content=read_file(fn)
   text = extract_field(content,"Text")
   questions_all += text + "\n" + "\n"   
 
-  #TOD0: Replace packages with usepackage{} and tikzpackage etc.
+  packages_list = extract_packages(content,packages_list)
+
+
+for cmd in list(packages_list.keys()): #for each command
+  packages_all = packages_all+"\\"+cmd+"{"+packages_list[cmd]+"}"+"\n"  #write and next line
+
+
  
- 
+"""Read template and replace Questions and packages""" 
 with open(template_name, 'r', encoding='utf-8') as file: #read file
      content_template = file.read()
 
 
 content_template=content_template.replace("QUESTIONSHERE",questions_all)     
-print(content_template)
-#TODO: write to a new file
+content_template=content_template.replace("PACKAGESHERE",packages_all)     
+
+
+with open(output_name,'w') as file: #read file
+     file.write(content_template)
+
 
           
         
