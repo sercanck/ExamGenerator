cbd916a92189ee7803ef814fdac01266965e131b
Sercan Cikintoglu <cikintoglus@itu.edu.tr>
2025-08-17 12:39:33 +0300

    Overall difficulty mode is added.

--- exam.py
+++ exam.py
@@ -2,16 +2,31 @@ import glob
 import os
 import re
 import random
+import numpy as np
 
-#TODO:Tüm dosyaları okumak zahmetli grep gibi bir sey olsa iyi is gorurdu
+#TODO:Tüm dosyaları okumak zahmetli grep gibi bir sey olsa iyi is gorurdu ama yok.
+#TODO:Include graphic
 
+
+target_overall_difficulty = 0
+
+
+if target_overall_difficulty>0:
+  mode=2
+else:
+  mode=1
+
+sigma=5.0    #Sigma of the Gaussian distrubition used in the 2nd mode
 
 pn_questions  = "./questions/"  #Name of the question folder
 template_name = "template.tex"  #Name of the exam's template
 output_name   = "exam.tex"      #Name of the exam
 
-question_list = []  
-packages_list = dict()
+
+question_list   = []   
+question_dict = dict()   #Required for 2nd mode
+difficulty_dict = dict() #Required for 2nd mode
+packages_dict = dict()
 questions_all = ""        
 packages_all  = ""
 
@@ -49,7 +64,7 @@ def extract_packages(content,plist):
       command, arguments = line.split(": ")
 
 
-    if command in list(packages_list.keys()): #If the command exists
+    if command in list(plist.keys()): #If the command exists
       for package in arguments.split(","):
         if package not in plist[command]:
           plist[command] = plist[command]+","+package
@@ -59,7 +74,10 @@ def extract_packages(content,plist):
     return plist
 
 
-with open('exam_structure','r') as file_structure:
+if mode==1: #Questions are selected based on individual difficulty       
+
+  print("Questions will be selected based on individual difficulties.")
+  with open('exam_structure','r') as file_structure:
     for line in file_structure.readlines(): #Loop over target question
       question, target_keyword, target_difficulty = line.rstrip("\n").split(": ")
       possible_questions = []
@@ -73,8 +91,7 @@ with open('exam_structure','r') as file_structure:
         content=read_file(fn)
         Keywords   = extract_field(content,"Keywords")
         Difficulty = extract_field(content,"Difficulty")
- 
-        
+    
         if "," in target_keyword: #If target keyword is plural
           if (target_difficulty==Difficulty) and set(target_keyword.split(",")).issubset(set(Keywords.split(", "))) :
             possible_questions.append(qn) 
@@ -82,44 +99,128 @@ with open('exam_structure','r') as file_structure:
           if (target_difficulty==Difficulty) and (target_keyword in Keywords):           
             possible_questions.append(qn)
 
-
       if possible_questions: 
         question_list.append(random.choice(possible_questions))      
       else:  
         print("Cannot find a suitable question candidate for "+question+"!!!")  #if possible_questions is empty do something...
-                  
 
-"""Merge questions and packages"""        
-for qn in question_list:
+  """Merge questions and packages"""        
+  for qn in question_list:
   
-  fn=pn_questions+qn
-  content=read_file(fn)
-  text = extract_field(content,"Text")
-  questions_all += text + "\n" + "\n"   
+    fn=pn_questions+qn
+    content=read_file(fn)
+    text = extract_field(content,"Text")
+    questions_all += text + "\n" + "\n"   
 
-  packages_list = extract_packages(content,packages_list)
+    packages_dict = extract_packages(content,packages_dict)
 
-
-for cmd in list(packages_list.keys()): #for each command
-  packages_all = packages_all+"\\"+cmd+"{"+packages_list[cmd]+"}"+"\n"  #write and next line
-
+  for cmd in list(packages_dict.keys()): #for each command
+    packages_all = packages_all+"\\"+cmd+"{"+packages_dict[cmd]+"}"+"\n"  #write and next line
 
  
-"""Read template and replace Questions and packages""" 
-with open(template_name, 'r', encoding='utf-8') as file: #read file
+  """Read template and replace Questions and packages""" 
+  with open(template_name, 'r', encoding='utf-8') as file: #read file
      content_template = file.read()
 
 
-content_template=content_template.replace("QUESTIONSHERE",questions_all)     
-content_template=content_template.replace("PACKAGESHERE",packages_all)     
+  content_template=content_template.replace("QUESTIONSHERE",questions_all)     
+  content_template=content_template.replace("PACKAGESHERE",packages_all)     
 
 
-with open(output_name,'w') as file: #read file
+  with open(output_name,'w') as file: #write file
      file.write(content_template)
 
 
-          
-        
+
+elif mode==2: #Questions are selected based on overall difficulty
+  print("Questions will be selected based on overall difficulty ("+str(target_overall_difficulty)+")")
+  with open('exam_structure','r') as file_structure:
+    possible_questions       = {} #it is a dictionary
+    possible_difficulties    = {} #it is a dictionary    
+    
+    for line in file_structure.readlines(): #Loop over target question
+      question, target_keyword, target_difficulty = line.rstrip("\n").split(": ")
+
+      possible_questions.setdefault(question, [])      #create a key in dict
+      possible_difficulties.setdefault(question, [])   #create a key in dict
+      question_dict.setdefault(question, [])   #create a key in dict
+      difficulty_dict.setdefault(question, [])   #create a key in dict      
+       
+      """Find suitable questions"""
+      for qn in sorted(os.listdir(pn_questions)):
+      
+        fn=pn_questions+qn
+        content=read_file(fn)
+        Keywords   = extract_field(content,"Keywords")
+        Difficulty = extract_field(content,"Difficulty")
+    
+        if "," in target_keyword: #If target keyword is plural
+          if set(target_keyword.split(",")).issubset(set(Keywords.split(", "))) : 
+            possible_questions[question].append(qn)
+            possible_difficulties[question].append(Difficulty)
+        else: #If target keyword is singular
+          if target_keyword in Keywords:           
+            possible_questions[question].append(qn)
+            possible_difficulties[question].append(Difficulty)
+
+  sorted_possible_questions = sorted(possible_questions.items(), key=lambda item: len(item[1]))
+      
+            
+  current_overall_difficulty = 0
+  selected_question         = 0
+
+  for question, possible_q_list in sorted_possible_questions:
+    if possible_q_list: #There are canditates
+      target_difficulty= target_overall_difficulty*(selected_question+1) - current_overall_difficulty*selected_question 
+      possible_d_list = np.array(possible_difficulties[question],dtype=float)
+            
+      d_prop = np.exp(-(possible_d_list-target_difficulty)**2/sigma) 
+      d_prop = d_prop/sum(d_prop)  #Normaliaze to 1
+
+      index_q = int( np.random.choice(np.arange(0,len(possible_d_list),1,dtype=int),1,p=d_prop))      #Select the index of the question
+      
+      question_dict[question] = possible_q_list[index_q]
+      difficulty_dict[question] = possible_d_list[index_q]
+      
+      current_overall_difficulty = ( current_overall_difficulty*selected_question + possible_d_list[index_q] ) / (selected_question+1)
+      selected_question +=1
+    else:
+      print("Cannot find a suitable question candidate for "+question+"!!!")  #if possible_questions is empty do something...
+
+  print("##############")      
+  print("Overall difficulty of the exam: "+str(current_overall_difficulty))    
+      
+  """Merge questions and packages"""        
+  for qn in question_dict.values():
+    if qn:
+      fn=pn_questions+qn
+      content=read_file(fn)
+      text = extract_field(content,"Text")
+      questions_all += text + "\n" + "\n"   
+
+      packages_dict = extract_packages(content,packages_dict)
+
+  for cmd in list(packages_dict.keys()): #for each command
+    packages_all = packages_all+"\\"+cmd+"{"+packages_dict[cmd]+"}"+"\n"  #write and next line
+
+ 
+  """Read template and replace Questions and packages""" 
+  with open(template_name, 'r', encoding='utf-8') as file: #read file
+     content_template = file.read()
+
+
+  content_template=content_template.replace("QUESTIONSHERE",questions_all)     
+  content_template=content_template.replace("PACKAGESHERE",packages_all)     
+
+
+  with open(output_name,'w') as file: #write file
+     file.write(content_template)
+  
+  with open('exam.log','w') as file:
+    file.write("Question  Difficulty\n\n")
+    for question, difficulty in difficulty_dict.items():
+       print(question+'  '+str(difficulty))
+       file.write(question+8*' '+str(difficulty)+'\n')
 
 
 
